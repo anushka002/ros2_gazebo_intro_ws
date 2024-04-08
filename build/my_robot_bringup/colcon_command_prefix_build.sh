70097a902ee6a5b1d43732c0964463b782a56998
anushka <anoushkasatav002@gmail.com>
2024-04-08 15:25:43 +0530

    first commit

---
+++ build/my_robot_bringup/colcon_command_prefix_build.sh
@@ -0,0 +1,2 @@
+# generated from colcon_core/shell/template/command_prefix.sh.em
+. "/home/anushka/ros2_beginner_2/robot_ws/install/my_robot_description/share/my_robot_description/package.sh"
---
+++ build/my_robot_description/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/anushka/ros2_beginner_2/robot_ws/src/my_robot_description
+# Build directory: /home/anushka/ros2_beginner_2/robot_ws/build/my_robot_description
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
